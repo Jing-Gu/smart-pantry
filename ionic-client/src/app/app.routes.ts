3ce552bbccc1baa356fc348771ba79545a9e44a0
jing-gu <jinggu.dev@gmail.com>
2024-12-27 16:05:11 -0500

    🚚 chore: refac routing

--- ionic-client/src/app/app.routes.ts
+++ ionic-client/src/app/app.routes.ts
@@ -1,16 +1,45 @@
 import { Routes } from "@angular/router";
+import { TabsPage } from "./tabs/tabs.page";
+import { PantryPage } from "./pantry/pantry.page";
+import { PantryItemComponent } from "./pantry/pantry-item/pantry-item.component";
+import { ShoppingPage } from "./shopping/shopping.page";
 
 export const routes: Routes = [
   {
+    path: "tabs",
+    component: TabsPage,
+    children: [
+      {
+        path: "pantry",
+        children: [
+          {
+            path: "",
+            component: PantryPage,
+          },
+          {
+            path: "add",
+            component: PantryItemComponent,
+          },
+          {
+            path: ":uuid",
+            component: PantryItemComponent,
+          },
+        ],
+      },
+      {
+        path: "shopping",
+        component: ShoppingPage,
+      },
+      {
+        path: "",
+        redirectTo: "/tabs/pantry",
+        pathMatch: "full",
+      },
+    ],
+  },
+  {
     path: "",
-    loadChildren: () => import("./tabs/tabs.routes").then((m) => m.routes),
-  },
-  {
-    path: "pantry",
-    loadComponent: () => import("./pantry/pantry.page").then((m) => m.PantryPage),
-  },
-  {
-    path: "shopping",
-    loadComponent: () => import("./shopping/shopping.page").then((m) => m.ShoppingPage),
+    redirectTo: "/tabs/pantry",
+    pathMatch: "full",
   },
 ];
